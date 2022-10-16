076e55e4f931170083dd8adb06888708c76f75d0
hitto <hitto@DESKTOP-A07E2BV>
2022-10-16 23:41:56 +0800

    加入專案檔案。

---
+++ Exec_For01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For01
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 5; i >=0 ; i--)
+			{
+				int space = 5 - i;
+				int star = i;
+				string row = new string(' ', space) + new string('*', star);
+				Console.WriteLine(row);
+			}
+		}
+	}
+}
